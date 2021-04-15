d5e98fc56369b1489271932b8b2785c0410f4e8e
Matheus_Olivan <matheus.neves@penseavanti.com.br>
2021-04-15 14:02:48 -0300

    Funfou. Descobrir como estilizar agora.

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
 import  MyModal  from '../Modal';
 import PropTypes from "prop-types";
-import { ResponsiveRadar } from '@nivo/radar'
+import {
+    Chart,
+    Tooltip,
+    Interval,
+    Coord
+} from "bizcharts";
 
 
 const Container =({
@@ -78,56 +83,20 @@ const Container =({
                     <div className='result-div'>
                         {console.log(user)}
                         <div className='chart-div'>
-                        <ResponsiveRadar
-                            data={addResposta}
-                            keys={['valor']}
-                            indexBy="texto"
-                            maxValue="auto"
-                            margin={{ top: 70, right: 180, bottom: 40, left: 180 }}
-                            curve="linearClosed"
-                            borderWidth={2}
-                            borderColor={{ from: 'color', modifiers: [] }}
-                            gridLevels={4}
-                            gridShape="circular"
-                            gridLabelOffset={35}
-                            enableDots={true}
-                            dotSize={6}
-                            dotColor={{ theme: 'background' }}
-                            dotBorderWidth={2}
-                            dotBorderColor={{ from: 'color' }}
-                            enableDotLabel={true}
-                            dotLabel="value"
-                            dotLabelYOffset={-12}
-                            colors={{ scheme: 'category10' }}
-                            fillOpacity={0.5}
-                            blendMode="multiply"
-                            animate={true}
-                            motionConfig="wobbly"
-                            isInteractive={true}
-                            // width={500}
-                            // height={500}
-                            legends={[
-                                {
-                                    anchor: 'top-left',
-                                    direction: 'column',
-                                    translateX: -50,
-                                    translateY: -40,
-                                    itemWidth: 80,
-                                    itemHeight: 20,
-                                    itemTextColor: '#999',
-                                    symbolSize: 12,
-                                    symbolShape: 'circle',
-                                    effects: [
+                            <Chart height={400} padding="auto" data={addResposta} autoFit>
+                                <Interval
+                                    adjust={[
                                         {
-                                            on: 'hover',
-                                            style: {
-                                                itemTextColor: '#000'
-                                            }
-                                        }
-                                    ]
-                                }
-                            ]}
-                        />
+                                            type: 'dodge',
+                                            marginRatio: 1,
+                                        },
+                                    ]}
+                                    color="name"
+                                    position="texto*valor"
+                                />
+                                <Coord type="polar"/>
+                                <Tooltip shared />
+                            </Chart>
                         </div>
                         <button className='playAgain-button' onClick={deNovoOnClick}>Reiniciar Avaliação</button>
                     </div>
